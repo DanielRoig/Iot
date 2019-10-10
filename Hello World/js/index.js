630a58f66d0bd0c30577c01dc123865ca65a021c
Daniel <daniielroig@gmail.com>
2019-10-10 11:47:30 +0200

    Eliminat el Hello World

--- Hello World/js/index.js
+++ Hello World/js/index.js
@@ -1,33 +1,4 @@
-var progress = 10;
 var myDB;
-
-
-//Esto es un "Listener", cada vez que se produce un cambio 
-//en un elemento indicado, ejecuta agluna cosa
-var button1 = document.getElementById("button1");
-button1.addEventListener("click", function () {
-	var element = document.getElementById("progress");
-	if (progress <= 100) {
-		progress += 10;
-		element.style.width = progress + "%";
-	}
-});
-
-
-//Esto es un "Listener", cada vez que se produce un cambio 
-//en un elemento indicado, ejecuta agluna cosa
-var checkbox = document.getElementById("customSwitch1");
-checkbox.addEventListener("click", function () {
-	if (checkbox.checked) {
-
-		document.getElementById("button1").className = "btn btn-warning btn-lg mb-4";
-		document.getElementById("progress").className = "progress-bar progress-bar-striped progress-bar-animated bg-warning";
-	} else {
-		document.getElementById("button1").className = "btn btn-primary btn-lg mb-4";
-		document.getElementById("progress").className = "progress-bar progress-bar-striped progress-bar-animated";
-	}
-
-});
 
 createDB();
 
