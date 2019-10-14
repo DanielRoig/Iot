6c416e3b37bd7ea9dead9ca01201ec846f5feb8c
Jordi Paradis <jordi.paradis@vendoservices.com>
2019-10-14 13:11:23 +0200

    Adding IOT simulator and changing default DB

--- Hello World/js/DB.js
+++ Hello World/js/DB.js
@@ -7,20 +7,24 @@ function createDB() {
 	}
 	
 	myDB.transaction(function (tran) {
-	
-		tran.executeSql('DROP TABLE Products');
-		tran.executeSql('DROP TABLE Brands');
-
-		tran.executeSql('CREATE TABLE IF NOT EXISTS Products (ID INTEGER PRIMARY KEY, Name VARCHAR, BrandID INTEGER, FOREIGN KEY (BrandID) REFERENCES Brands(ID))');
-		tran.executeSql('CREATE TABLE IF NOT EXISTS Brands (ID INTEGER PRIMARY KEY, Name VARCHAR)');
-
-		tran.executeSql('insert into Brands (Name) values ("Apple")');
-		tran.executeSql('insert into Brands (Name) values ("Samsung")');
 		
-		tran.executeSql('insert into Products (Name, BrandID) values ("iPhone", 1)');
-		tran.executeSql('insert into Products (Name, BrandID) values ("iPad", 1)');
-		tran.executeSql('insert into Products (Name, BrandID) values ("Galaxy S10", 2)');
-		tran.executeSql('insert into Products (Name, BrandID) values ("Galaxy Fold", 2)');
+		var p=new Promise(function(){
+			tran.executeSql('DROP TABLE Products');
+			tran.executeSql('DROP TABLE Brands');
+		});
 
+		Promise.resolve().then(() => {
+
+			tran.executeSql('CREATE TABLE IF NOT EXISTS Products (ID INTEGER PRIMARY KEY, Name VARCHAR, BrandID INTEGER, FOREIGN KEY (BrandID) REFERENCES Brands(ID))');
+			tran.executeSql('CREATE TABLE IF NOT EXISTS Brands (ID INTEGER PRIMARY KEY, Name VARCHAR)');
+
+			tran.executeSql('insert into Brands (Name) values ("Apple")');
+			tran.executeSql('insert into Brands (Name) values ("Samsung")');
+		
+			tran.executeSql('insert into Products (Name, BrandID) values ("iPhone", 1)');
+			tran.executeSql('insert into Products (Name, BrandID) values ("iPad", 1)');
+			tran.executeSql('insert into Products (Name, BrandID) values ("Galaxy S10", 2)');
+			tran.executeSql('insert into Products (Name, BrandID) values ("Galaxy Fold", 2)');
+		});
 	});
 }
