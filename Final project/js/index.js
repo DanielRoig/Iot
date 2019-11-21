caf4cb8408d1ea052ca9f57b7c93bcf1b982fa31
Daniel <daniielroig@gmail.com>
2019-11-21 20:01:27 +0100

    car function working

--- Final project/js/index.js
+++ Final project/js/index.js
@@ -10,137 +10,118 @@ myDB.transaction(function (tran) {
 			element.innerHTML = data.rows[i].Name;
 			element.value = data.rows[i].Name;
 
-/* 			document.getElementById("createDropdown").appendChild(element.cloneNode(true));
-			document.getElementById("readDropdown").appendChild(element.cloneNode(true));
-			document.getElementById("deleteDropdownBrands").appendChild(element.cloneNode(true));
- */
+			/* 			document.getElementById("createDropdown").appendChild(element.cloneNode(true));
+						document.getElementById("readDropdown").appendChild(element.cloneNode(true));
+						document.getElementById("deleteDropdownBrands").appendChild(element.cloneNode(true));
+			 */
 		}
 	});
 });
 
-/* var createButton = document.getElementById("createButton");
-createButton.addEventListener("click", function () {
-	var valueDropdown = document.getElementById("createDropdown");
-	var product = document.getElementById("createInput").value;
-	if (product != "") {
-		myDB.transaction(function (tran) {
-			tran.executeSql("SELECT ID FROM Brands WHERE Name='" + valueDropdown.options[valueDropdown.selectedIndex].value + "'", [], function (tran, data) {
-				tran.executeSql('insert into Products (Name, BrandID) values ("' + product + '", ' + data.rows[0].ID + ')');
-			});
 
-		});
-		document.getElementById("createInput").value = "";
-	}
-}); */
+function createNewLabel(nameBrand, plate, nameDriver) {
+	var element = document.createElement("a");
+	element.className = "list-group-item list-group-item-action";
+	element.id = "list-home-list";
+	element.href = `#${nameDriver}${nameBrand}`;
 
-/* var createButtonBrand = document.getElementById("createButtonBrand");
-createButtonBrand.addEventListener("click", function () {
-	var brand = document.getElementById("createInputBrand").value;
-	if (brand != "") {
-		myDB.transaction(function (tran) {
-			tran.executeSql('insert into Brands (Name) values ("' + brand + '")');
+	element.setAttribute("data-toggle", "list");
+	element.setAttribute("role", "tab");
+	element.setAttribute("aria-controls", "home");
 
-			var element = document.createElement("option");
-			element.innerHTML = brand;
-			element.value = brand;
+	element.innerHTML = `Car: <b>${nameBrand}</b> License plate: <b>${plate}</b>`;
 
-			document.getElementById("createDropdown").appendChild(element.cloneNode(true));
-			document.getElementById("readDropdown").appendChild(element.cloneNode(true));
-			document.getElementById("deleteDropdownBrands").appendChild(element.cloneNode(true));
-		});
-		document.getElementById("createInputBrand").value = "";
-	}
-}); */
+	document.getElementById("list-tab").appendChild(element);
+	createNewDetail(nameBrand, plate, nameDriver)
+}
 
-/* var readDropdown = document.getElementById("readDropdown")
-readDropdown.addEventListener('change', (event) => {
-	var textBox = document.getElementById("textBox")
 
-	while (textBox.hasChildNodes()) {
-		textBox.removeChild(textBox.lastChild);
-	}
 
-	myDB.transaction(function (tran) {
-		tran.executeSql("SELECT ID FROM Brands WHERE Name='" + readDropdown.options[readDropdown.selectedIndex].value + "'", [], function (tran, data) {
-			tran.executeSql('SELECT Name FROM Products WHERE PRODUCTS.BrandID=' + data.rows[0].ID, [], function (tran, data) {
-				for (var i = 0; i < data.rows.length; i++) {
-					var element = document.createElement("li");
-					element.innerHTML = data.rows[i].Name;
-					element.value = data.rows[i].Name;
-					element.className = "list-group-item";
-					textBox.appendChild(element);
-				}
-			});
-		});
-	});
-}); */
 
-/* 
-var deleteDropdown = document.getElementById("deleteDropdownBrands")
-deleteDropdown.addEventListener('change', (event) => {
+function createNewDetail(nameBrand, plate, nameDriver) {
+	var element = document.createElement("a");
+	element.className = "tab-pane fade";
+	element.id = `${nameDriver}${nameBrand}`;
 
-	var options = document.getElementById("deleteDropdownProducts");
+	element.setAttribute("role", "tabpanel");
+	element.setAttribute("aaria-labelledby", "list-messages-list");
 
-	for (var i = 1; i < options.childElementCount; i++) {
-		options.removeChild(options.options[i]);
-	}
+	element.innerHTML = `<div class="card active">
+	<div class="card-body">
+	  <h5 class="card-title">${nameBrand} ${plate}</h5>
+	  <ul class="list-group">
+		<li class="list-group-item d-flex justify-content-between align-items-center">
+		  CocaCola
+		  <span class="badge badge-primary badge-pill">1</span>
+		</li>
+		<li class="list-group-item d-flex justify-content-between align-items-center">
+		  Spaghetti
+		  <span class="badge badge-primary badge-pill">2</span>
+		</li>
+		<li class="list-group-item d-flex justify-content-between align-items-center">
+		  Meat
+		  <span class="badge badge-primary badge-pill">1</span>
+		</li>
+	  </ul>
 
-	myDB.transaction(function (tran) {
-		tran.executeSql("SELECT ID FROM Brands WHERE Name='" + event.target.value + "'", [], function (tran, data) {
-			tran.executeSql('SELECT Name FROM Products WHERE PRODUCTS.BrandID=' + data.rows[0].ID, [], function (tran, data) {
-				for (var i = 0; i < data.rows.length; i++) {
+	  <dl class="row mt-3">
+		<dt class="col-sm-3">Description</dt>
+		<dd class="col-sm-9">A description list is perfect for defining terms.</dd>
 
-					var element = document.createElement("option");
-					element.innerHTML = data.rows[i].Name;
-					element.value = data.rows[i].Name;
+		<dt class="col-sm-3">Euismod</dt>
+		<dd class="col-sm-9">
+		  <p>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</p>
+		  <p>Donec id elit non mi porta gravida at eget metus.</p>
+		</dd>
 
-					document.getElementById("deleteDropdownProducts").appendChild(element.cloneNode(true));
-				}
-			});
-		});
-	});
-}); */
+		<dt class="col-sm-3">Malesuada </dt>
+		<dd class="col-sm-9">Etiam porta sem malesuada magna mollis euismod.</dd>
+	  </dl>
 
-/* var deleteButton = document.getElementById("deleteButton");
-deleteButton.addEventListener("click", function () {
+	  <div class="text-right mt-3">
+		<button class="btn btn-outline-danger">Delete order</button>
+		<button class="btn btn-success">Done</button>
+	  </div>
+	</div>
+  </div>`;
 
-	var deleteDropdownProducts = document.getElementById("deleteDropdownProducts");
-	myDB.transaction(function (tran) {
-		tran.executeSql("DELETE FROM Products WHERE Name = '" + deleteDropdownProducts.options[deleteDropdownProducts.selectedIndex].value + "'", [], function (tran, data) {
-			deleteDropdownProducts.removeChild(deleteDropdownProducts.options[deleteDropdownProducts.selectedIndex]);
-			
-		});
-	});
-}); */
+	document.getElementById("nav-tabContent").appendChild(element);
+}
+
+createNewLabel("opel", 4362346, "juan");
+createNewLabel("citroen", 54654694, "marc");
+createNewLabel("ferrari", 654746, "david");
+
+
+
 
 var epc = []
 var x = new XMLHttpRequest();
 x.open("GET", "http://localhost:3161/devices/simulator/inventory/", true);
 x.onreadystatechange = function () {
-  if (x.readyState == 4 && x.status == 200)
-  {
-    var doc = x.responseXML;
-	  console.log(doc);
-      var size = doc.getElementsByTagName("size")[0].firstChild.nodeValue;
-	  console.log(size);
-      var AdvanNetId = doc.getElementsByTagName("advanNetId")[0].firstChild.nodeValue;
-	  console.log(AdvanNetId);
-          
-      // We read all the epcs
-      for(var i=0; i < doc.getElementsByTagName("epc").length; i++){
-		  epc.push(doc.getElementsByTagName("epc")[i].childNodes[0].nodeValue);
-      }
-	  console.log(epc);
-  }
+	if (x.readyState == 4 && x.status == 200) {
+		var doc = x.responseXML;
+		console.log(doc);
+		var size = doc.getElementsByTagName("size")[0].firstChild.nodeValue;
+		console.log(size);
+		var AdvanNetId = doc.getElementsByTagName("advanNetId")[0].firstChild.nodeValue;
+		console.log(AdvanNetId);
+
+		// We read all the epcs
+		for (var i = 0; i < doc.getElementsByTagName("epc").length; i++) {
+			epc.push(doc.getElementsByTagName("epc")[i].childNodes[0].nodeValue);
+		}
+		console.log(epc);
+	}
 };
 
 x.send(epc);
 
-myDB.transaction (function (tran,id){
+myDB.transaction(function (tran, id) {
 	var trx = "";
-	id=epc;
-	tran.executeSql('insert into Cars (ID, Name, License, OrderID ) values (?, "Toyota", "477663 RTT", ?)',[id[0], id[1]])
-	trx= tran.executeSql('select * from Cars');
+	id = epc;
+	tran.executeSql('insert into Cars (ID, Name, License, OrderID ) values (?, "Toyota", "477663 RTT", ?)', [id[0], id[1]])
+	trx = tran.executeSql('select * from Cars');
 	console.log(trx);
 });
 
@@ -148,4 +129,3 @@ myDB.transaction (function (tran,id){
 
 // open -n -a /Applications/Google\ Chrome.app --args --user-data-dir="/Users/jordi/unirepos/Iot/IoT-simulator-2.3.18_02" --disable-web-security
 // $ open -a Google\ Chrome --args --disable-web-security --/Users/jordi/unirepos/Iot/Chrome_test
-
