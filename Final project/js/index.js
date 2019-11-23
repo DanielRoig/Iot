b09435ccedc103f8143e103d7cb8e25968d81381
Daniel <daniielroig@gmail.com>
2019-11-23 18:32:59 +0100

    Emulator linked , duplication prevention

--- Final project/js/index.js
+++ Final project/js/index.js
@@ -5,66 +5,57 @@ createDB();
 
 function createPost(id) {
 
-	var data1;
+	if (id != NaN) {
+		var data1;
+		myDB.transaction(function (tran) {
+			tran.executeSql(`SELECT * FROM Cars WHERE ID=${id}`, [], function (tran, data) {
+				for (var i = 0; i < data.rows.length; i++) {
+					data1 = data.rows[i];
+				}
 
-	myDB.transaction(function (tran) {
-		tran.executeSql(`SELECT * FROM Cars WHERE ID=${id}`, [], function (tran, data) {
-			for (var i = 0; i < data.rows.length; i++) {
-				data1 = data.rows[i];
-			}
-
-			tran.executeSql(`SELECT * FROM Products WHERE CarID=${id}`, [], function (tran, data) {
-				createNewLabel(data1.CarName, data1.LicenseNumber, data1.DriverName, data1.DescriptionOrder, data);
+				tran.executeSql(`SELECT * FROM Products WHERE CarID=${id}`, [], function (tran, data) {
+					//drivername carname
+					if (!document.getElementById(`${data1.DriverName}${data1.CarName}`)) {
+						createNewLabel(data1.CarName, data1.LicenseNumber, data1.DriverName, data1.DescriptionOrder, data);
+					}
+				});
 			});
 		});
-	});
+	}
 }
 
-createPost(1);
-createPost(2);
-createPost(3);
-createPost(4);
+//createPost(1);
+//createPost(2);
+//createPost(3);
+//createPost(4);
 
 
 
 
-const interval = setInterval (function(){
-	var epc = []
+const interval = setInterval(function () {
+	var epc;
 	var id = []
 	var x = new XMLHttpRequest();
 	x.open("GET", "http://localhost:3161/devices/simulator/inventory/", true);
 	x.onreadystatechange = function () {
 		if (x.readyState == 4 && x.status == 200) {
 			var doc = x.responseXML;
-			console.log(doc);
+
 			// var size = doc.getElementsByTagName("size")[0].firstChild.nodeValue;
 			// console.log(size);
 			var AdvanNetId = doc.getElementsByTagName("advanNetId")[0].firstChild.nodeValue;
-			console.log(AdvanNetId);
-	
+
+
 			// We read all the epcs
 			for (var i = 0; i < doc.getElementsByTagName("epc").length; i++) {
-				epc.push(doc.getElementsByTagName("epc")[i].childNodes[0].nodeValue);
+				epc = doc.getElementsByTagName("epc")[i].childNodes[0].nodeValue;
+				console.log(parseInt(epc));
+				createPost(epc);
 			}
-			console.log(parseInt(epc));
-			epc=parseInt(epc);
 		}
 	};
-	x.send(epc);
-	createPost(epc[0]);
-},500);
-
-
-
-// myDB.transaction(function (tran, id) {
-// 	var trx = "";
-// 	id = epc;
-// 	tran.executeSql('insert into Cars (ID, Name, License, OrderID ) values (?, "Toyota", "477663 RTT", ?)', [id[0], id[1]])
-// 	trx = tran.executeSql('select * from Cars');
-// 	console.log(trx);
-// });
-
-
+	x.send();
+}, 500);
 
 // open -n -a /Applications/Google\ Chrome.app --args --user-data-dir="/Users/jordi/unirepos/Iot/IoT-simulator-2.3.18_02" --disable-web-security
 // $ open -a Google\ Chrome --args --disable-web-security --/Users/jordi/unirepos/Iot/Chrome_test
