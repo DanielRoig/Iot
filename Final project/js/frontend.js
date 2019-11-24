cf181b7f46d73b1dc8aad0c83e7f66e1811220e3
Daniel <daniielroig@gmail.com>
2019-11-24 11:14:43 +0100

    Modal system almost done

--- Final project/js/frontend.js
+++ Final project/js/frontend.js
@@ -1,38 +1,38 @@
 function createNewLabel(CarName, LicenseNumber, DriverName, Description, ProductsObject) {
-    var element = document.createElement("a");
-    element.className = "list-group-item list-group-item-action";
-    element.id = `${DriverName}${CarName}Label`;
-    element.href = `#${DriverName}${CarName}Detail`;
+  var element = document.createElement("a");
+  element.className = "list-group-item list-group-item-action";
+  element.id = `${DriverName}${CarName}Label`;
+  element.href = `#${DriverName}${CarName}Detail`;
 
-    element.setAttribute("data-toggle", "list");
-    element.setAttribute("role", "tab");
-    element.setAttribute("aria-controls", "home");
+  element.setAttribute("data-toggle", "list");
+  element.setAttribute("role", "tab");
+  element.setAttribute("aria-controls", "home");
 
-    element.innerHTML = `Car: <b>${CarName}</b> License plate: <b>${LicenseNumber}</b>`;
+  element.innerHTML = `Car: <b>${CarName}</b> License plate: <b>${LicenseNumber}</b>`;
 
-    document.getElementById("list-tab").appendChild(element);
-    var listElements = '';
-    for (var i = 0; i < ProductsObject.rows.length; i++) {
-        listElements+= `<li class="list-group-item d-flex justify-content-between align-items-center">
+  document.getElementById("list-tab").appendChild(element);
+  var listElements = '';
+  for (var i = 0; i < ProductsObject.rows.length; i++) {
+    listElements += `<li class="list-group-item d-flex justify-content-between align-items-center">
         ${ProductsObject.rows[i].ProductName}
         <span class="badge badge-primary badge-pill">${ProductsObject.rows[i].NumProd}</span>
       </li>`
-    }
+  }
 
-    createNewDetail(CarName, LicenseNumber, DriverName, Description, listElements)
+  createNewDetail(CarName, LicenseNumber, DriverName, Description, listElements)
 }
 
 
 
 function createNewDetail(CarName, LicenseNumber, DriverName, Description, ProductArray) {
-    var element = document.createElement("a");
-    element.className = "tab-pane fade";
-    element.id = `${DriverName}${CarName}Detail`;
+  var element = document.createElement("a");
+  element.className = "tab-pane fade";
+  element.id = `${DriverName}${CarName}Detail`;
 
-    element.setAttribute("role", "tabpanel");
-    element.setAttribute("aaria-labelledby", "list-messages-list");
+  element.setAttribute("role", "tabpanel");
+  element.setAttribute("aaria-labelledby", "list-messages-list");
 
-    element.innerHTML = `<div class="card active">
+  element.innerHTML = `<div class="card active">
 	<div class="card-body">
 	  <h5 class="card-title">${CarName} ${LicenseNumber}</h5>
 	  <ul class="list-group" id="list">
@@ -49,31 +49,37 @@ function createNewDetail(CarName, LicenseNumber, DriverName, Description, Produc
 
 	  <div class="text-right mt-3">
 		<button class="btn btn-outline-danger" id="${DriverName}${CarName}DeleteButton" value="${DriverName}">Delete order</button>
-		<button class="btn btn-success" id="${DriverName}${CarName}DoneButton" value="${DriverName}">Done</button>
+		<button class="btn btn-success" id="${DriverName}${CarName}DoneButton" data-drivername="${DriverName}" data-toggle="modal" data-target="#doneModal">Done</button>
 
 	  </div>
 	</div>
   </div>`;
 
-    document.getElementById("nav-tabContent").appendChild(element);
+  document.getElementById("nav-tabContent").appendChild(element);
 
-    var deleteButton = document.getElementById(`${DriverName}${CarName}DeleteButton`);
-    deleteButton.addEventListener("click", (event) => {
-      deleteModal(event.target.value)
-    });
 
-    var doneButton = document.getElementById(`${DriverName}${CarName}DoneButton`);
-    doneButton.addEventListener("click", (event) => {
-      doneModal(event.target.value)
-    });
+  $('#doneModal').on('show.bs.modal', function (e) {
+
+    var drivername = $(e.relatedTarget).data('drivername');
+ 
+    $(e.currentTarget).find('div[class="modal-body"]').text(drivername);
+
+    $(e.currentTarget).find('button[class="btn btn-success done"]').val(drivername);
+
+  });
+}
+
+function deleteFromDB(name){
 
 }
 
-function deleteModal(DriverName){
-  console.log(DriverName)
+function deleteFromFrontEnd(){
+
 }
 
-function doneModal(DriverName){
-  console.log(DriverName)
-}
+$('.done').on('click', function (e) {
 
+  var drivername = e.currentTarget.value;
+
+
+});
